96b053b063f9c9bd0133c52e88bb667b865586df
Anastasia Rodionova <31624075+nans1996@users.noreply.github.com>
2018-10-20 23:38:38 +0300

    переходы

---
+++ El_Protecting/Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeInOut : MonoBehaviour
+{
+
+  //  public static bool sceneEnd;
+    public float fadeSpeed;
+    //public int nextLevel;
+    private Image _image;
+    private bool sceneStarting;
+
+    void Awake()
+    {
+        _image = GetComponent<Image>();
+        _image.enabled = true;
+        sceneStarting = true;
+       // sceneEnd = false;
+        Cursor.visible = false;
+        _image.color = Color.white;
+    }
+
+    void Update()
+    {
+       // _image.color = Color.Lerp(_image.color, Color.white, fadeSpeed * Time.deltaTime / 2);
+      /*  if (sceneStarting)*/ StartScene();
+       // if (sceneEnd) EndScene();
+    }
+
+    void StartScene()
+    {
+    _image.color = Color.Lerp(_image.color, Color.clear, fadeSpeed * Time.deltaTime*2);
+
+        if (_image.color.a <= 0.01f)
+        {
+            _image.color = Color.clear;
+            _image.enabled = false;
+            sceneStarting = false;
+            Cursor.visible = true;
+        }
+    }
+
+    //void EndScene()
+    //{
+    //    _image.enabled = true;
+    //    _image.color = Color.Lerp(_image.color, Color.black, fadeSpeed * Time.deltaTime);
+
+    //    if (_image.color.a >= 0.95f)
+    //    {
+    //        Cursor.visible = false;
+    //        _image.color = Color.black;
+    //       // Application.LoadLevel(nextLevel);
+    //    }
+    //}
+}
--- El_Protecting/Assets/Scripts/Transition.cs
+++ El_Protecting/Assets/Scripts/Transition.cs
@@ -1,11 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Transition : MonoBehaviour {
 
-	public void TransitionToProfile()
+    public Image img;
+    public AnimationCurve curve;
+
+    public void TransitionToProfile()
     {
         Application.LoadLevel("profile");
     }
+
+    public void TransitionToWelcome()
+    { 
+        Application.LoadLevel("Welcom");
+       
+    }
+
+    public void TransitionToQuestion()
+    {
+        Application.LoadLevel("question");
+        
+        
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 0f; // сначала она 100% не прозрачна, поэтому t = 1
+
+        while (t < 1f)
+        {
+            t -= Time.deltaTime * 1.5f; // отнимаем от t с каждым циклом время после посл. фрейма пока t не станет 0 или меньше. Тут я еще умнажаю на 1.5, тут уже смотрите как вам точнее нужно
+            float a = curve.Evaluate(t); // теперь с помощью кривой оцениваем значение "a"
+            img.color = new Color(0.28f, 0.5f, 0.78f, a); // и обновляем прозрачность картинки с помощью "a"
+            yield return 0;
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        float t = 1f; // сначала она 100% не прозрачна, поэтому t = 1
+
+        while (t > 0f)
+        {
+            t -= Time.deltaTime * 1.5f; // отнимаем от t с каждым циклом время после посл. фрейма пока t не станет 0 или меньше. Тут я еще умнажаю на 1.5, тут уже смотрите как вам точнее нужно
+            float a = curve.Evaluate(t); // теперь с помощью кривой оцениваем значение "a"
+            img.color = new Color(0.28f, 0.5f, 0.78f, a); // и обновляем прозрачность картинки с помощью "a"
+            yield return 0;
+        }
+    }
+
+
 }
